4c8f1bbc5210ac79d1b58a5ba54a06ec731f39aa
atafs <americo.tomas@fuseuniversal.com>
2018-02-09 17:22:20 +0000

    49. Breather and Review

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import 'babel-polyfill';
 import express from 'express';
+
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
+
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 // router handler
 const app = express();
@@ -7,7 +13,17 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  // logic to initialize and load data into the store
+  const store = createStore();
+  console.info(matchRoutes(Routes, req.path));
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null;
+  });
+  console.info('promises', promises);
+  Promise.all(promises).then(() => {
+    // render the applications
+    res.send(renderer(req, store));
+  });
 });
 
 app.listen(3000, () => {
